6af728d20c889d56ed49908ab045e319d6c2c6f9
Dmitry Ponyatov <dponyatov@gmail.com>
2017-04-02 11:19:47 +0400

    *+ Base Class Instance
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- 500L.py
+++ 500L.py
@@ -1,10 +1,23 @@
 # http://www.aosabook.org/en/500L/a-simple-object-model.html
 
-class Class:
-    def __init__(self, name, super=None):
-        self.name = name ; self.super = super
+class Base(object):
+    tag = 'Base'
+    def __init__(self, name, base=None, meta=None, fields={}):
+        self.name = name ; self.base = base ; self.meta = meta
+        self.fields = fields
     def __repr__(self):
-        return '%s:%s(%s)' % (self.__class__.__name__, self.name, self.super)
+        return '%s:%s'%(self.tag,self.name)
+#         return '[ %s:%s base:%s meta:%s ]' % \
+#             (self.tag, self.name, self.base, self.meta)
+
+class Class(Base): tag = 'Class' 
+
+class Instance(Base):
+    tag = 'Instance'
+#     def __init__(self,base): Base.__init__(self, base.name, base, base.meta)
+    
+OBJECT = Class('Object')
+TYPE = Class('Type')
 
 def test_rw_field():
     # Python
@@ -16,4 +29,7 @@ def test_rw_field():
     assert obj.a == 1
     assert obj.b == 5
     # SOM
-    A = Class('A') ; assert '%s' % A == 'Class:A(None)'
+    A = Class(name='A',base=OBJECT,meta=TYPE)
+    assert '%s' % A == 'Class:A'
+    obj = Instance(A)
+    assert '%s' % obj == 'Instance:Class:A'
