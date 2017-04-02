b206881d885d48554c9781d5fa21331642098b51
Dmitry Ponyatov <dponyatov@gmail.com>
2017-04-02 20:57:39 +0400

    + OBJECT/TYPE + test_rw_class_fields
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- 500L.py
+++ 500L.py
@@ -26,12 +26,17 @@ class Class(Base):
 class Instance(Base):
     ' Instance of a user-defined class '
     def __init__(self,cls):
+        assert isinstance(cls, Class)
         Base.__init__(self, cls, {})
     def __repr__(self):
         return '%s:%s' % (self.__class__.__name__, self.cls)
     
-OBJECT = None#Class('Object')
-TYPE = None#Class('Type')
+OBJECT = Class('Object', base=None, meta=None)
+TYPE = Class('Type', base=OBJECT, meta=None)
+# TYPE is an instance of itself
+TYPE.cls = TYPE
+# OBJECT is an instance of TYPE
+OBJECT.cls = TYPE
 
 def test_rw_field():
     # Python
@@ -59,3 +64,15 @@ def test_rw_field():
     assert obj.Rattr('a') == 2
     assert obj.Rattr('b') == 5
     
+def test_rw_class_fields():
+    # Python
+    class A(object): pass
+    A.a = 1
+    assert A.a == 1
+    A.a = 6
+    assert A.a == 6
+    # object model
+    A = Class('A', base=OBJECT, meta=TYPE, fields={'a':1})
+    assert A.Rattr('a') == 1
+    A.Wattr('a', 6)
+    assert A.Rattr('a') == 6
