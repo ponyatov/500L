a64a2c8cbdf928063aa3307f3544551645cd9008
Dmitry Ponyatov <dponyatov@gmail.com>
2017-04-02 10:59:54 +0400

    + Class(Base)
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- 500L.py
+++ 500L.py
@@ -1,5 +1,9 @@
 # http://www.aosabook.org/en/500L/a-simple-object-model.html
 
+class Class:
+    def __init__(self, name): self.name = name
+    def __repr__(self): return '%s:%s' % (self.__class__.__name__, self.name)
+
 def test_rw_field():
     # Python
     class A(object): pass
@@ -10,4 +14,4 @@ def test_rw_field():
     assert obj.a == 1
     assert obj.b == 5
     # SOM
-    
+    A = Class('A') ; assert '%s' % A == 'Class:Az'
