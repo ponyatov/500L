1a55f84f94bac130b0e5d59d9e5cb38cabd97776
Dmitry Ponyatov <dponyatov@gmail.com>
2017-04-02 11:33:57 +0400

    * Base/Class/Instance
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- 500L.py
+++ 500L.py
@@ -1,23 +1,32 @@
 # http://www.aosabook.org/en/500L/a-simple-object-model.html
 
 class Base(object):
-    tag = 'Base'
-    def __init__(self, name, base=None, meta=None, fields={}):
-        self.name = name ; self.base = base ; self.meta = meta
-        self.fields = fields
+    ' The base class that all of the object model classes inherit from '
+    def __init__(self, cls, fields):
+        self.cls = cls ; self._fields = fields
     def __repr__(self):
-        return '%s:%s'%(self.tag,self.name)
-#         return '[ %s:%s base:%s meta:%s ]' % \
-#             (self.tag, self.name, self.base, self.meta)
+        return '%s:%s' % (self.__class__.__name__, self.name)
+    def wattr(self, fldname, value):
+        self.wdict(fldname, value)
+    def wdict(self, fldname, value):
+        self._fields[fldname] = value
 
-class Class(Base): tag = 'Class' 
+class Class(Base):
+    ' A User-defined class '
+    def __init__(self, name, base, meta, fields={}):
+        Base.__init__(self, meta, fields)
+        self.name = name
+        self.base = base
 
 class Instance(Base):
-    tag = 'Instance'
-#     def __init__(self,base): Base.__init__(self, base.name, base, base.meta)
+    ' Instance of a user-defined class '
+    def __init__(self,cls):
+        Base.__init__(self, cls, {})
+    def __repr__(self):
+        return '%s:%s' % (self.__class__.__name__, self.cls)
     
-OBJECT = Class('Object')
-TYPE = Class('Type')
+OBJECT = None#Class('Object')
+TYPE = None#Class('Type')
 
 def test_rw_field():
     # Python
@@ -29,7 +38,8 @@ def test_rw_field():
     assert obj.a == 1
     assert obj.b == 5
     # SOM
-    A = Class(name='A',base=OBJECT,meta=TYPE)
+    A = Class(name='A', base=OBJECT, meta=TYPE)
     assert '%s' % A == 'Class:A'
     obj = Instance(A)
     assert '%s' % obj == 'Instance:Class:A'
+    obj.wattr('a', 1)
